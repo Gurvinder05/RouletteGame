a2dc0b8e4dafed3c506058078fdf9793a4cf0874
Gurvinder Singh <GS060@atcltd.atc.org.nz>
2020-10-16 13:40:03 +1300

    Russianroulette

---
+++ UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RussianRoulette;
+namespace UnitTestProject1
+{
+    
+    [TestClass]
+    public class UnitTest1
+    {
+        CreateRouletteGame TestingObj = new CreateRouletteGame();
+        int numPlayers = 2;
+        [TestMethod]
+        public void GamePlayingMethod()
+        {
+            WinLose obj = new WinLose();
+            int startChamber = TestingObj.spinChamber();
+            int bulletChamber = TestingObj.spinChamber();
+            int ChamberPosition = TestingObj.Playing(numPlayers, startChamber, bulletChamber);
+            if (ChamberPosition == -1)
+            {
+                Assert.AreEqual(obj.WinGame, obj.WinGame);
+                Assert.AreEqual(obj.LoseGame, obj.LoseGame);
+            }
+            else
+            {
+                Assert.AreEqual("NextRound", "NextRound");
+
+            }
+        }
+        [TestMethod]
+        public void GenrateBuletteAndChamberNumberInGun()
+        {
+            int startChamber = TestingObj.spinChamber();
+            int bulletChamber = TestingObj.spinChamber();
+        }
+    }
+}
